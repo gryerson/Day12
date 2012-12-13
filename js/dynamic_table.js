df4ac30811434bbb2e5dca46cbdf33f6e98c2599
gryerson <guy.ryerson@gmail.com>
2012-12-13 11:47:21 -0500

    fourth commit

--- js/dynamic_table.js
+++ js/dynamic_table.js
@@ -1,52 +1,35 @@
-// Declare counter variable
-var counter = 0;
+// Bind page load function
+$(document).ready( function() {	
 
-// Add contact function adds new table rows
-function addContact()
-{
-	// Get input from the user
-	var inputName = document.getElementById('name').value;
-	var inputEmail = document.getElementById('email').value; 
-	var inputPhone = document.getElementById('phone').value; 
-	
-	// Create new HTML row elements
-	var contactRow = document.createElement("tr");
-	
-	var idRow = document.createElement("td");
-	var nameRow = document.createElement("td");
-	var emailRow = document.createElement("td");
-	var phoneRow = document.createElement("td");
-	var removeButtonRow = document.createElement("td");
-	
-	// Set row value
-	idRow.innerHTML = counter + 1;
-	
-	// Set input values
-	nameRow.innerHTML = inputName;
-	emailRow.innerHTML = inputEmail;
-	phoneRow.innerHTML = inputPhone;
-	
-	// Set remove button
-	removeButtonRow.innerHTML = '<button onclick="removeContact(this.parentNode.parentNode.rowIndex);">Delete</button>';
-	
-	// Add table cells
-	contactRow.appendChild(idRow);
-	contactRow.appendChild(nameRow);
-	contactRow.appendChild(emailRow);
-	contactRow.appendChild(phoneRow);
-	contactRow.appendChild(removeButtonRow);
-	
-	// Add table row to the table
-	document.getElementById('contacts').appendChild(contactRow);
-	
-	// Increment counter
-	counter++;
-	
+    // Declare counter variable
+    var counter = 0;
 
-} // end addContact()
+    // Bind Delete contact function removes table rows
+    $('#delContact').live( 'click',function() {
+        $(this).parent().parent().remove();		
+    }); // end bind function remove rows
 
-// Delete contact function removes existing table rows
-function removeContact(i)
-{
-    document.getElementById('contacts').deleteRow(i);	
-} // end deleteContact() function
+    // Bind Add contact function adds new table rows
+    $('#addContact').click(function() {
+    	// Get input from the user
+    	var inputName = $('#name').val();
+    	var inputEmail = $('#email').val();
+    	var inputPhone = $('#phone').val();	
+
+    	// Set row value 
+    	var idRow = counter+1;
+
+    	// create table cells and rows
+    	$('tbody').append("<tr>  
+    	   <td>"+idRow+"<td> 
+    	   <td>"+inputName+"</td>  
+    	   <td>"+inputEmail+"</td> 
+    	   <td>"+inputPhone+"</td>  
+    	   <td><button id='delContact'>Delete</button><td> 
+    	<tr>");  
+
+    	// Increment counter
+    	counter++;
+    	});  end bind function add rows
+
+}); // end page load
